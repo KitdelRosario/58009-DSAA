02e397cb9b7d42bf96cedfa0c6bb748e0a69261f
KitdelRosario <124062219+KitdelRosario@users.noreply.github.com>
2023-09-06 13:18:32 +0800

    Add files via upload

---
+++ Append.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numbers[] = {1,2,3,4,5,6,7,8,9,10};
+	int size = sizeof(numbers) / sizeof(numbers[0]);
+	
+	int insertValue = 11;
+	int insertIndex = 10;
+	
+	for(int i = size; i >insertIndex; i--){
+		numbers[i] = numbers[i-1];
+	}
+	
+	numbers[insertIndex] = insertValue;
+	
+	for(int i = 0; i<size +1; i++){
+		cout<< numbers[i]<<" ";
+	}
+	cout<<endl;
+}
